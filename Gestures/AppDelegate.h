56038c7b2f273401ff8d008c6b392fe185c92ffd
Tim Hanby <teh@trifecta.com>
2014-02-27 09:23:49 -0500

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by Tim Hanby on 2/27/14.
+//  Copyright (c) 2014 Timothy Hanby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gestures/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gestures
+//
+//  Created by Tim Hanby on 2/27/14.
+//  Copyright (c) 2014 Timothy Hanby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
